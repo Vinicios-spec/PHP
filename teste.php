381a76b53a5922e624557a4784efbbabe03df0e9
Vinicios Martinez Ferreira Cruz <vinimariobros@gmail.com>
2026-04-15 17:15:07 -0300

    Strings

---
+++ teste.php
@@ -0,0 +1,8 @@
+<?php
+$nome = "vinicios";
+//echo 'O Valor da variável, $nome é = a Wilton';
+echo "O valor da variável '$nome' é = {$nome}";
+//Interpolação em PHP é o recurso que insere automaticamente o valor de variáveis dentro de strings delimitadas por aspas duplas
+
+/**/
+?>
